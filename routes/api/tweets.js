4a0288b6edde7e94ebe472b15808e8a8ba35da34
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-15 17:40:07 -0700

    add more routes

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -4,33 +4,33 @@ const jwt = require("jsonwebtoken");
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-const Tweet = require('../../models/Tweet');
 const validateTweetInput = require('../../validation/tweets');
+const Tweet = require('../../models/Tweet');
 
-// router.get('/', (req, res) => {
-//   Tweet.find()
-//     .sort({ date: -1 })
-//     .then(tweets => res.json(tweets))
-//     .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
-// });
+router.get('/', (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then(tweets => res.json(tweets))
+    .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+});
 
-// router.get('/user/:user_id', (req, res) => {
-//   Tweet.find({ user: req.params.user_id })
-//     .sort({ date: -1 })
-//     .then(tweets => res.json(tweets))
-//     .catch(err =>
-//       res.status(404).json({ notweetsfound: 'No tweets found from that user' }
-//       )
-//     );
-// });
+router.get('/user/:user_id', (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .sort({ date: -1 })
+    .then(tweets => res.json(tweets))
+    .catch(err =>
+      res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+      )
+    );
+});
 
-// router.get('/:id', (req, res) => {
-//   Tweet.findById(req.params.id)
-//     .then(tweet => res.json(tweet))
-//     .catch(err =>
-//       res.status(404).json({ notweetfound: 'No tweet found with that ID' })
-//     );
-// });
+router.get('/:id', (req, res) => {
+  Tweet.findById(req.params.id)
+    .then(tweet => res.json(tweet))
+    .catch(err =>
+      res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+    );
+});
 
   
 router.post('/',
