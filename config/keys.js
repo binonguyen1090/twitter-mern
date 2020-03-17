5039410cdf16b573d988a48b11265256753926c9
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-16 19:33:38 -0700

    fix gitignore

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://admin:zRd6dw3SZTmTOaGD@cluster0-3ibcp.mongodb.net/test?retryWrites=true&w=majority",
+//     secretOrKey: 'secret'
+// }
+
+// keys.js
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  })
+} else {
+  module.exports = require("./keys_dev");
+}
