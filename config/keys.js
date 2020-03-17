942d200fba3f26fc732e81bde4fdee20f3d873d1
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-16 20:17:59 -0700

    testing heroku

--- config/keys.js
+++ config/keys.js
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_dev');
 }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
