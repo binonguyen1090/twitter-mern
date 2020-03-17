180d44b62024bfae55bddc04e56b1ab7d5d1b20b
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-16 20:03:53 -0700

    trying to push to heroku

--- config/keys.js
+++ config/keys.js
@@ -2,14 +2,17 @@
 //     mongoURI: "mongodb+srv://admin:zRd6dw3SZTmTOaGD@cluster0-3ibcp.mongodb.net/test?retryWrites=true&w=majority",
 //     secretOrKey: 'secret'
 // }
-const express = require("express");
-const app = express();
+
 // keys.js
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  })
-} else {
-  module.exports = require("./keys_dev");
+  });
 }
